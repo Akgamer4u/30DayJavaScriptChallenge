93bafa6f461ac6ab76303e6a140742afe2be5ab7
Avinash Kumar <avinashkumar848117@gmail.com>
2024-08-01 18:04:42 +0530

    new change

---
+++ day19.js
@@ -0,0 +1,52 @@
+// const str="I love javascript coding language and I am learning javascript";
+// const regex=/javascript/gi;
+// const matches=str.match(regex);
+// console.log(matches)
+
+
+// const str="I like the number 1234 and 0987 and 4567 this number is my favorite number";
+// const regex=/\d+/gi;
+// const matches=str.match(regex);
+// console.log(matches)
+
+// const str="Javascript is a programming language";
+// const regex=/\b[A-Z][a-z]*\b/gi;
+// const matches=str.match(regex);
+// console.log(matches)
+
+
+// const str="My phone number is (123) 456-7890.";
+// const regex=/\(\d{3}\)\s\d{3}-\d{4}/gi;
+// const matches=str.match(regex);
+// console.log(matches)
+
+// const str="My email is user@example.com.";
+// const regex=/\w+@\w+\.\w+/gi;
+// const matches=str.match(regex); 
+// console.log(matches)
+
+// const str="Hello world. This is JavaScript.";
+// const regex=/^\w+/gi;
+// const matches=str.match(regex);
+// console.log(matches)
+
+
+// const str="Hello world. This is JavaScript.";
+// const regex=/\w+$/gi;
+// const matches=str.match(regex);
+// console.log(matches)
+
+// const password = "Avinashk00@";
+// const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+// const testing = regex.test(password);
+// console.log(testing);
+// const matches = password.match(regex);
+// console.log(matches);
+
+// const url="https://www.example.com";
+// const regex=/^(https?:\/\/)?(www\.)?[\w-]+\.\w{2,}(\.\w{2,})?$/;
+
+// const testing=regex.test(url);
+// console.log(testing)
+// const matches=url.match(regex);
+// console.log(matches)
